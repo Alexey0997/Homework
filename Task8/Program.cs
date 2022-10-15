5b46d28f16899f1bb9353f7b22b6d9def58f28a2
alexey <asivlev@yandex.ru>
2022-10-15 14:01:17 +0300

    Задача 8 диапазон четных чисел

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число N, 
+// а на выходе показывает все четные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Уважаемый пользователь, введите целое положительное число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 2; // Поскольку это первое четное число после 1.
+Console.WriteLine($"В диапазоне от {1} до {number} имеются следующие четные числа: ");
+while(count <= number)
+{
+    Console.Write($"{count}, ");
+    count = count + 2;
+}
